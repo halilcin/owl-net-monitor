26fbea3c8df52022d3a2c05e689a9f8dd1139fc2
Halil Cin <halilcin.pl@gmail.com>
2025-03-24 04:30:54 +0300

    "Fixed speed values"

--- cinwork.py
+++ cinwork.py
@@ -17,7 +17,7 @@ def index():
 def owl_bandwidth():
     old_stats = psutil.net_io_counters()
     while True:
-        eventlet.sleep(1) 
+        time.sleep(1) 
         new_stats = psutil.net_io_counters()
 
         upload_speed = (new_stats.bytes_sent - old_stats.bytes_sent) / 1024  # KB/s
